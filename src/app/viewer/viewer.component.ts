4448fa9417ab014de53f17d155551ee5708393b2
bennyboer <barny.eder@gmail.com>
2020-11-01 18:44:07 +0100

    Supporting file dropping

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,4 +1,14 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone, OnDestroy, OnInit, ViewChild} from "@angular/core";
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	ElementRef,
+	HostListener,
+	NgZone,
+	OnDestroy,
+	OnInit,
+	ViewChild
+} from "@angular/core";
 import {BoxGeometry, Camera, Geometry, Material, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
 import {Helper} from "dxf";
 import {ControlsComponent} from "./controls/controls.component";
@@ -16,6 +26,16 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewerComponent implements OnInit, OnDestroy {
+
+	/**
+	 * Message shown as placeholder when no file has been loaded.
+	 */
+	private static readonly PLACEHOLDER_MESSAGE = "Please select a CAD file to load";
+
+	/**
+	 * Message shown when a file is dragged over the viewer component.
+	 */
+	private static readonly RELEASE_MOUSE_TO_DROP_MESSAGE = "Release the mouse to load the file";
 
 	/**
 	 * Controls of the viewer.
@@ -37,6 +57,11 @@ export class ViewerComponent implements OnInit, OnDestroy {
 	 * Whether the placeholder should be shown.
 	 */
 	public showPlaceholder: boolean = true;
+
+	/**
+	 * Currently shown placeholder message.
+	 */
+	public placeHolderMessage: string = ViewerComponent.PLACEHOLDER_MESSAGE;
 
 	constructor(
 		private readonly cd: ChangeDetectorRef,
@@ -147,6 +172,60 @@ export class ViewerComponent implements OnInit, OnDestroy {
 	 */
 	public ngOnInit(): void {
 		this.initializeControlBindings();
+	}
+
+	/**
+	 * Called on drop on the component.
+	 * @param event of the drop
+	 */
+	@HostListener("drop", ["$event"])
+	public onDrop(event: DragEvent): void {
+		event.preventDefault(); // Prevent the default action (opening link or something)
+
+		if (!!event.dataTransfer && !!event.dataTransfer.files && event.dataTransfer.files.length >= 1) {
+			const file: File = event.dataTransfer.files[0];
+
+			this.showCADFile(file);
+		}
+	}
+
+	/**
+	 * Called on drag over the component.
+	 * @param event of the drag over
+	 */
+	@HostListener("dragover", ["$event"])
+	public onDragOver(event: DragEvent): void {
+		event.preventDefault(); // Allow dropping files
+	}
+
+	/**
+	 * Called on drag enter on the component.
+	 * @param event of the drag enter
+	 */
+	@HostListener("dragenter", ["$event"])
+	public onDragEnter(event: DragEvent): void {
+		if (!!event.target && event.target instanceof HTMLElement) {
+			const target: HTMLElement = event.target;
+			if (target.className === "placeholder") {
+				// Set "you are allowed to drop" message
+				this.placeHolderMessage = ViewerComponent.RELEASE_MOUSE_TO_DROP_MESSAGE;
+			}
+		}
+	}
+
+	/**
+	 * Called on drag leave on the component.
+	 * @param event of the drag leave
+	 */
+	@HostListener("dragleave", ["$event"])
+	public onDragLeave(event: DragEvent): void {
+		if (!!event.target && event.target instanceof HTMLElement) {
+			const target: HTMLElement = event.target;
+			if (target.className === "placeholder") {
+				// Reset drop message
+				this.placeHolderMessage = ViewerComponent.PLACEHOLDER_MESSAGE;
+			}
+		}
 	}
 
 }
